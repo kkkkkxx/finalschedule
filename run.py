382f1d3150925e7bd46fb4c2c7eab6a4bdddcb76
George Saieed <George@MacBook-Pro-3.local>
2016-05-04 12:01:21 -0500

    Add procfile

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-# Download the twilio-python library from http://twilio.com/docs/libraries
-from twilio.rest import TwilioRestClient
+from flask import Flask, request, redirect
+import twilio.twiml
 
-# Find these values at https://twilio.com/user/account
-account_sid = "AC8d3cd606442775ed86d1e6cd26f3cd43"
-auth_token = "457ee4916f5c5ed892e83dc37c924b45"
-client = TwilioRestClient(account_sid, auth_token)
+app = Flask(__name__)
 
-message = client.messages.create(to="+4124179805", from_="+14122534757",
-                                     body="Hello there!!!")
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
