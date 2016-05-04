8ff44872e6501dd9234b6e9702504fb9698485fe
George Saieed <George@MacBook-Pro-3.local>
2016-05-04 12:47:29 -0500

    Start adding class

--- run.py
+++ run.py
@@ -9,6 +9,16 @@ callers = {
     "+19542269211": "Philip",
 }
 
+finals = {
+    "AANL 10103 1":["Elementary Hittite (3)", "Wednesday", "June 8th", "OR 315", "8:00 AM", "10:00 AM"],
+    "AKKD 10103 1":["Elementary Akkadian (3)", "Monday", "June 6th", "OR 210", "10:30 AM", "12:30 PM"],
+    "ANTH 21420 1":["Ethnographic Methods", "Thursday", "June 9th", "HM 104", "1:30 PM", "3:30 PM"],
+    "ARAB 10103 1":["Elementary Arabic (3)", "Friday", "June 10th", "HM 145", "10:30 AM", "12:30 PM"],
+    "ARAB 10103 2":["Elementary Arabic (3)", "Monday", "June 6th", "HM 145", "10:30 AM", "12:30 PM"],
+    "ARAB 10103 3":["Elementary Arabic (3)", "Friday", "June 10th", "HM 145", "1:30 PM", "3:30 PM"],
+}
+
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond and greet the caller by name."""
@@ -17,7 +27,7 @@ def hello_monkey():
     if from_number in callers:
         message = callers[from_number] + ", thanks for the message!"
     else:
-        message = "Monkey, thanks for the message!"
+        message = "Please make sure you have the correct format! Text [Dept. Code] [Class Number] [Section Number]. For example, 'CMSC 11000 1'. You can find your section number at http://classes.uchicago.edu"
 
     resp = twilio.twiml.Response()
     resp.message(message)
